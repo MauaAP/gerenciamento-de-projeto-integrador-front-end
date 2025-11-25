17ab0511faafee4e76489cc39ee28374c2b826ea
Gabriel Merola <gabrielmerola12@gmail.com>
2025-11-25 01:11:49 -0300

    feat: update project title and enhance UI with custom scrollbar styles and toast notifications

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Login from "./pages/default/login";
 import PrivateADMRoutes from "./utils/PrivateADMRoutes";
 import PrivateMODRoutes from "./utils/PrivateMODRoutes";
@@ -49,6 +51,18 @@ export default function App() {
 
                 </Routes>
             </BrowserRouter>
+            <ToastContainer
+                position="top-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
         </div>
     )
 }
