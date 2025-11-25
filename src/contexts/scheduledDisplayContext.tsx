31bfaf34a51038beb453450da28a1ec3f5983b65
Gabriel Merola <gabrielmerola12@gmail.com>
2025-11-25 01:13:04 -0300

    feat: update ScheduledDisplayContext to include setPresentations and reloadPresentations for better state management

--- src/contexts/scheduledDisplayContext.tsx
+++ src/contexts/scheduledDisplayContext.tsx
@@ -24,24 +24,28 @@ import type { PresentationInterface } from "../services/interface/PresentationIn
 
 interface ScheduledDisplayContextProps {
     presentations: PresentationInterface[];
+    setPresentations: Dispatch<SetStateAction<PresentationInterface[]>>;
 
     principalPresentation: PresentationInterface;
     setPrincipalPresentation: Dispatch<SetStateAction<PresentationInterface>>;
 
     carouselPresentations: PresentationInterface[];
     setCarouselPresentations: Dispatch<SetStateAction<PresentationInterface[]>>;
+
+    reloadPresentations?: () => Promise<void>;
 }
 
 export const ScheduledDisplayContext= createContext<ScheduledDisplayContextProps | undefined>(undefined);
 
-export const ScheduledDisplayContextProvider =({ children, presentationList } : { children: React.ReactNode, presentationList: PresentationInterface[]}) => {
-    const presentations = presentationList;
+export const ScheduledDisplayContextProvider =({ children, presentationList, reloadFn } : { children: React.ReactNode, presentationList: PresentationInterface[], reloadFn?: () => Promise<void>}) => {
+    const [presentations, setPresentations] = useState<PresentationInterface[]>(presentationList);
 
     const [principalPresentation, setPrincipalPresentation]= useState<PresentationInterface>(presentationList[0]);
 
     const [carouselPresentations, setCarouselPresentations]= useState<PresentationInterface[]>(presentationList.length > 1? presentationList.slice(1) : []);
 
     useEffect(() => {
+        setPresentations(presentationList);
         setPrincipalPresentation(presentationList[0]);
         setCarouselPresentations(presentationList.length > 1 ? presentationList.slice(1) : []);
         
@@ -51,10 +55,12 @@ export const ScheduledDisplayContextProvider =({ children, presentationList } : 
         <ScheduledDisplayContext.Provider
             value={{
                 presentations,
+                setPresentations,
                 principalPresentation,
                 setPrincipalPresentation,
                 carouselPresentations,
-                setCarouselPresentations
+                setCarouselPresentations,
+                reloadPresentations: reloadFn
             }}
         >
             {children}
